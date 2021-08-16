5084821a5b2e35a374136ab1525041744cb4801e
theSLWayne <hiranhasanka@gmail.com>
2021-08-17 01:57:58 +0530

    Add main and data loading functions

--- create_tfrecords.py
+++ create_tfrecords.py
@@ -4,6 +4,11 @@ import argparse
 import os
 
 import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+
+batch_size = 32
+img_size = (180, 180)
 
 def init_args():
     """
@@ -18,8 +23,6 @@ def init_args():
             help = 'Path to the image dataset', required = True)
     parser.add_argument('-m', '--mode', type = str, 
             help = 'Mode: Whether to create tfrecords for Train or Test set.', default = 'Train')
-    parser.add_argument('-v', '--validation_portion', type = float,
-            help = 'Enter the partition that should be used to create the validation set', default = 0.2)
 
     return parser.parse_args()
 
@@ -34,5 +37,61 @@ def validate_args(args):
 
     assert os.path.isdir(args.dataset_path), 'Invalid Argument: -p / --dataset_path should be a valid folder path'
     assert args.mode.lower() in ['train', 'test'], 'Invalid Argument: -m / --mode should be either train or testt'
-    assert args.validation_portion >= 0.0 and args.validation < 1.0, 'Invalid Argument: -v / --validation_portion should be a float between 1 and 0.'
 
+def load_data(dataset_path):
+    '''
+    
+    Loads train/test dataset from a directory and creates tensorflow datasets.
+
+    :param dataset_path: Path to the directory containing training/test data
+    :return: Train/test dataset as tf.data.Dataset object
+    '''
+
+    ds = tf.keras.preprocessing.image_dataset_from_directory(
+        dataset_path,
+        image_size = img_size,
+        batch_size = batch_size
+    )
+
+    return ds
+
+def write_tfrecords(dataset, save_path, mode):
+    '''
+    
+    Writes the given dataset into the given directory as a tfrecords file.
+
+    :param dataset: The dataset that needs to be written into tfrecords file
+    :param save_path: The path of the directory where tfrecords files will be saved to
+    :param mode: Dataset mode; Train or Test
+    :return: Filepath of the written tfrecords file
+    '''
+
+    # Create filepath for the tfrecords file
+    tfrecords_file_path = os.path.join(save_path, "{}.tfrecords".format(mode.lower()))
+
+    # Classes list
+    class_list = dataset.class_names
+
+    for batch in tqdm(dataset):
+        for image, label in zip(batch[0], batch[1]):
+            pass
+
+
+# main function
+if __name__ == '__main__':
+    '''
+    
+    Run the script
+    '''
+
+    # Get arguments
+    args = init_args()
+
+    # Validate arguments
+    validate_args(args)
+
+    # Load dataset from folder
+    dataset = load_data(args.dataset_path)
+
+    # Write dataset to tfrecord files
+    saved_file_path = write_tfrecords(dataset, "", args.mode)
