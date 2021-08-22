4f9656d8319210b0fcad433a8eb4b7f7e9cf70d7
theSLWayne <hiranhasanka@gmail.com>
2021-08-23 02:16:06 +0530

    Function to read dataset from tfrecords file

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def init_args():
     parser.add_argument('-d', '--dataset_path', type = str,
             help = 'Path to the tfrecords file containing training dataset', required = True)
     parser.add_argument('-m', '--model_save_path', type = str,
-            help = 'Path of the directory where trained model should be saved to', required = True)
+            help = 'Path of the directory where trained model should be saved to', required = False)
     parser.add_argument('-e', '--epochs', type = int, help = 'Number of epochs to train')
 
     return parser.parse_args()
@@ -50,6 +50,38 @@ def validate_args(args):
     """
 
     assert os.path.exists(args.dataset_path) and args.dataset_path.endswith('.tfrecords'), 'Invalid Argument: -d / --dataset_path should be a valid path to a tfrecords file'
+
+def parse_image_func(example):
+    """
+    
+    Parses raw tfrecord example
+
+    :param example: Single example from tfrecords dataset
+    :return: Parsed example
+    """
+
+    # Set the feature description as same as the one in craete_tfrecords.py script
+    feature_description = {
+        'image': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64),
+    }
+
+    return tf.io.parse_single_example(example, feature_description)
+
+def read_data(path):
+    """
+    
+    Reads training data from tfrecords files
+
+    :param path: Path to tfrecord file containing training data
+    :return: Dataset loaded from the file
+    """
+
+    raw_dataset = tf.data.TFRecordDataset(path)
+
+    return raw_dataset.map(parse_image_func)
+
+
 
 def train_model():
     pass
@@ -68,4 +100,4 @@ if __name__ == '__main__':
     validate_args(args)
 
     # Train model
-
+    read_data(args.dataset_path)
